498da4fb7c8402a96590a3ba56cffae3aefac1b6
marcefuentes <85162146+marcefuentes@users.noreply.github.com>
2022-03-09 12:06:10 +0100

    Update listener.js

--- extension/listener.js
+++ extension/listener.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request !== 'getDSID') return false;
-  var cUrl = 'https://ssl.binghamton.edu/';
+  var cUrl = 'https://vpn.udc.es/';
   var cName = 'DSID';
 	chrome.cookies.get({url: cUrl, name: cName}, function(cookie) {
 	sendResponse(cookie);
